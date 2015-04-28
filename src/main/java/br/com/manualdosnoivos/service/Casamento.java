6d3db19d63677d6bb1db64a40d0cc164503b6b7f
Fabiano <fabianoyoschitaki@gmail.com>
2015-04-28 01:30:15 -0300

    first commit

---
+++ src/main/java/br/com/manualdosnoivos/service/Casamento.java
@@ -0,0 +1,22 @@
+package br.com.manualdosnoivos.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+@WebService
+public class Casamento {
+	@WebMethod(operationName="listarCasamentos")
+	public @WebResult(name="casamento")
+	List<String> listarCasamentos (
+	 @WebParam(name="data")String data){
+		List<String> retorno = new ArrayList<String>();
+		retorno.add("Maria e João");
+		retorno.add("Pirilampo e Matusalenha");
+		return retorno;
+	}
+}
