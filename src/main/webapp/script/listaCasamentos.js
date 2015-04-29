deca294607218333a80e30aca8b5a48dd0cf6250
Caio Franco <barroso.pc@gmail.com>
2015-04-28 22:06:47 -0300

    Testando Ajax do Angular

--- src/main/webapp/script/listaCasamentos.js
+++ src/main/webapp/script/listaCasamentos.js
@@ -1,12 +1,13 @@
 (function(){
 	var app = angular.module('listaCasamento', []);
 
-	app.controller('CasamentoController', function(){
-		this.casamentos = getListaCasamentos();
+	app.controller('CasamentoController', function($scope, $http){
+		this.casamentos = getListaCasamentos($http);
+		
 	});
 	
 	
-	function getListaCasamentos(){
+	function getListaCasamentos($http){
 		var casamentos = [];
 		
 		var soap = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.manualdosnoivos.com.br/\">"+
@@ -18,17 +19,26 @@
 					   "</soapenv:Body>"+
 					"</soapenv:Envelope>";
 		
-		var xhr = $.ajax({
-			url: "http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/Casamento",
-			type: "POST",
-			data: soap,
-			timeout: 15000,
-			//dataType: "text",
-			contentType: "text/xml;charset=UTF-8;SOAPAction=\"\"",
-			success: function(data) {
-			},
-			error: function(data) {
-			}
+		// var xhr = $.ajax({
+			// url: "http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/Casamento",
+			// type: "POST",
+			// data: soap,
+			// timeout: 15000,
+			// //dataType: "text",
+			// contentType: "text/xml;charset=UTF-8;SOAPAction=\"\"",
+			// success: function(data) {
+			// },
+			// error: function(data) {
+			// }
+		// });
+		
+		var responsePromise = $http.post("http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/Casamento",soap);
+
+		responsePromise.success(function(data, status, headers, config) {
+			console.log(data);
+		});
+		responsePromise.error(function(data, status, headers, config) {
+			console.log(data);
 		});
 		return casamentos;
 	}
