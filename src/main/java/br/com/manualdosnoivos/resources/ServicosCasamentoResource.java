75be7d7c4264368295f800d5c65092152b704104
Fabiano <fabianoyoschitaki@gmail.com>
2015-04-29 00:24:18 -0300

    serviço rest

--- src/main/java/br/com/manualdosnoivos/resources/ServicosCasamentoResource.java
+++ src/main/java/br/com/manualdosnoivos/resources/ServicosCasamentoResource.java
@@ -3,8 +3,10 @@ package br.com.manualdosnoivos.resources;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import br.com.manualdosnoivos.common.ServicoCasamento;
@@ -18,17 +20,33 @@ import br.com.manualdosnoivos.common.ServicoCasamento;
  *
  */
 @Path("/servicoscasamento")
+@Produces("application/json")
+@Consumes("application/json")
 public class ServicosCasamentoResource {
 
 	@GET
-	@Produces("application/json")
+	@Path("/servicos")
 	public Set<ServicoCasamento> getServicosCasamento(){
 		Set<ServicoCasamento> retorno = new HashSet<ServicoCasamento>();
 		retorno = getServicosCasamentoMock();
 		return retorno;
 	}
-
-	private Set<ServicoCasamento> getServicosCasamentoMock() {
+	
+	@GET
+	@Path("/servico/{nome}")
+	public ServicoCasamento getServicoCasamento(@PathParam("nome")String nome){
+		ServicoCasamento retorno = null;
+		Set<ServicoCasamento> todosServicos = getServicosCasamento();
+		for (ServicoCasamento servicoCasamento : todosServicos) {
+			if (nome.equals(servicoCasamento.getNome())){
+				retorno = servicoCasamento;
+				break;
+			}
+		}
+		return retorno;
+	}
+	
+	private static Set<ServicoCasamento> getServicosCasamentoMock() {
 		Set<ServicoCasamento> retorno = new HashSet<ServicoCasamento>();
 		retorno.add(new ServicoCasamento("Convites", "Crie convites personalizados para seu casamento!"));
 		retorno.add(new ServicoCasamento("Buffet", "Escolha o melhor buffet para sua festa!"));
