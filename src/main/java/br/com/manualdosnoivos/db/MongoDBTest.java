4658891201b092707526f84f377adadc96b62c0c
Fabiano <fabianoyoschitaki@gmail.com>
2015-05-01 18:42:18 -0300

    mongo

--- src/main/java/br/com/manualdosnoivos/db/MongoDBTest.java
+++ src/main/java/br/com/manualdosnoivos/db/MongoDBTest.java
@@ -1,15 +1,11 @@
 package br.com.manualdosnoivos.db;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 
 /**
  * Teste mongoDB no openshit
@@ -27,35 +23,30 @@ public class MongoDBTest {
 	private static final String DATABASE = "manualdosnoivos";
 	private static final String USER = "admin";
 	private static final String PASSWORD = "MWxQNVpaUjAw";
-	private static final String CONNECTION_URL = 
-		"mongodb://[admin:MWxQNVpaUjAw]localhost:27017/manualdosnoivos";
-	
-	//conexão
-	private static ServerAddress serverAddress = new ServerAddress(CONNECTION_URL);
-	private static List<MongoCredential> credentials = Arrays.asList(MongoCredential
-			.createCredential(USER, DATABASE, PASSWORD.toCharArray()));
+	private static final String CONNECTION_URL = "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/";
+
+	private static MongoCredential credential = MongoCredential
+			.createCredential(USER, DATABASE, PASSWORD.toCharArray());
 
 	public static String getCasamentos() {
 		StringBuffer retorno = new StringBuffer();
 		try {
-			retorno.append("MongoClient..");
-			// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-		       
-	        MongoClientURI uri  = new MongoClientURI(CONNECTION_URL); 
-	        MongoClient client = new MongoClient(uri);
-	        retorno.append("\nMongoClient generated");
-	        DB db = client.getDB(uri.getDatabase());
+			retorno.append("MongoClient...");
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
+			retorno.append("\nMongoClient generated");
+			DB db = mongoClient.getDB(DATABASE);
 			retorno.append("\nConnect to database successfully");
 			
 			DBCollection casamento = db.getCollection("casamentos");
 			retorno.append("\nCollection created successfully");
+			//TODO dando pau aqui
 			BasicDBObject doc = new BasicDBObject("data", "10/10/2015").
 	            append("noivo", "João").
 	            append("noiva", "Maria").
 	            append("custo", 21000);
 			casamento.insert(doc);
 			retorno.append("\nok");
-			client.close();
+			mongoClient.close();
 		} catch (Exception e) {
 			retorno.append("\n" + e.getClass().getName() + ": " + e.getMessage());
 		}
