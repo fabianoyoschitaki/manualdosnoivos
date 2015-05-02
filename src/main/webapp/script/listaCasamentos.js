ba3bfe6624657e839ddf77d2caa2311d12afe903
Caio Franco <barroso.pc@gmail.com>
2015-05-02 12:45:49 -0300

    Test Rest

--- src/main/webapp/script/listaCasamentos.js
+++ src/main/webapp/script/listaCasamentos.js
@@ -4,32 +4,33 @@
 	app.controller('CasamentoController', function($scope, $http){
 		this.casamentos = [];
 		
-		var soap = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.manualdosnoivos.com.br/\">"+
-					   "<soapenv:Header/>"+
-					  "<soapenv:Body>"+
-						  "<ser:listarCasamentos>"+
-							 "<data>?</data>"+
-						  "</ser:listarCasamentos>"+
-					   "</soapenv:Body>"+
-					"</soapenv:Envelope>";
+		// var soap = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.manualdosnoivos.com.br/\">"+
+					   // "<soapenv:Header/>"+
+					  // "<soapenv:Body>"+
+						  // "<ser:listarCasamentos>"+
+							 // "<data>?</data>"+
+						  // "</ser:listarCasamentos>"+
+					   // "</soapenv:Body>"+
+					// "</soapenv:Envelope>";
 		
-		var responsePromise = $http.post("http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/services/Casamento",soap);
-
+		// var responsePromise = $http.post("http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/services/Casamento",soap);
+		var responsePromise = $http.get("http://manualdosnoivos-manualdosnoivos.rhcloud.com/rest/servicoscasamento/servicos");
+	
 		responsePromise.success(function(data, status, headers, config) {
-			var xml = $.parseXML(data);
-			var casamentos = [];
-			$("casamento", xml).each(function(){
-				var casamento = {};
-				casamento.casal = $(this).text();
-				casamentos.push(casamento);
-			});
-			$scope.casamento.casamentos = casamentos;
+			console.log(data);
+			// var xml = $.parseXML(data);
+			// var casamentos = [];
+			// $("casamento", xml).each(function(){
+				// var casamento = {};
+				// casamento.casal = $(this).text();
+				// casamentos.push(casamento);
+			// });
+			// $scope.casamento.casamentos = casamentos;
 		});
 		responsePromise.error(function(data, status, headers, config) {
 			console.log(data);
 		});
 	});
-	
 	
 	function getListaCasamentos($scope, $http){
 		
