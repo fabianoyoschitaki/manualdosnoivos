1fa54e3d357f60fd4f25b1e88c01f8cf4b9a8bc1
Caio Franco <barroso.pc@gmail.com>
2015-05-02 12:56:56 -0300

    Separando em Arquivos

---
+++ src/main/webapp/script/defaultFunctions.js
@@ -0,0 +1,31 @@
+(function(){	
+	function getListaCasamentos($scope, $http){
+		var soap = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://service.manualdosnoivos.com.br/\">"+
+					   "<soapenv:Header/>"+
+					  "<soapenv:Body>"+
+						  "<ser:listarCasamentos>"+
+							 "<data>?</data>"+
+						  "</ser:listarCasamentos>"+
+					   "</soapenv:Body>"+
+					"</soapenv:Envelope>";
+		
+		var responsePromise = $http.post("http://manualdosnoivos-manualdosnoivos.rhcloud.com:80/services/Casamento",soap);
+		// var responsePromise = $http.get("http://manualdosnoivos-manualdosnoivos.rhcloud.com/rest/servicoscasamento/servicos");
+	
+		responsePromise.success(function(data, status, headers, config) {
+			// console.log(data);
+			var xml = $.parseXML(data);
+			var casamentos = [];
+			$("casamento", xml).each(function(){
+				var casamento = {};
+				casamento.casal = $(this).text();
+				casamentos.push(casamento);
+			});
+			$scope.casamento.casamentos = casamentos;
+		});
+		responsePromise.error(function(data, status, headers, config) {
+			console.log(data);
+		});
+		
+	}
+})()
