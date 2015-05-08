33034f47a3cbd8d3f4e97a64d79f547b13accf3a
Fabiano <fabianoyoschitaki@gmail.com>
2015-05-07 22:15:06 -0300

    lol

--- src/main/java/br/com/manualdosnoivos/resources/FornecedoresResource.java
+++ src/main/java/br/com/manualdosnoivos/resources/FornecedoresResource.java
@@ -6,7 +6,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -29,6 +31,22 @@ public class FornecedoresResource {
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public CategoriaFornecedor adicionaCategoriaFornecedor(CategoriaFornecedor c){
 		return new CategoriaFornecedorDAO().createCategoriaFornecedor(c);
+	}
+	
+	/**@GET
+	@Path("/categoria/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public CategoriaFornecedor deletaCategoriaFornecedor(
+		@PathParam(name="id") String id,
+		@QueryParam(name="operation") String operacao){
+		return new CategoriaFornecedorDAO().createCategoriaFornecedor(c);
+	}**/
+	
+	@DELETE
+	@Path("/categoria/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deletaCategoriaFornecedor(@PathParam("id") String id){
+		new CategoriaFornecedorDAO().deleteCategoriaFornecedor(id);
 	}
 	
 	@POST
